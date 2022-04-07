6a08c7d9328210c2ca24905e1c6a75d569729537
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-04-07 13:51:34 +0530

    refactor: barrel exports for assets

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -3,5 +3,13 @@ import ProfessionalWomenSvg from "./cheerful-curly-business-girl-wearing-glasses
 import TeamSvg from "./team.svg";
 import PersonalGrowth from "./personal-growth.svg";
 import DrumSetSvg from "./drum-set.svg";
+import BlackCatFace from "./cat-black-face.png";
 
-export { AppSvg, ProfessionalWomenSvg, TeamSvg, PersonalGrowth, DrumSetSvg };
+export {
+  AppSvg,
+  ProfessionalWomenSvg,
+  TeamSvg,
+  PersonalGrowth,
+  DrumSetSvg,
+  BlackCatFace,
+};
