f9666b0ceba3d2a794afe37db6c41218b91ef505
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-04-07 13:37:21 +0530

    added alert component

--- src/components/AlertText/AlertText.jsx
+++ src/components/AlertText/AlertText.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { useAlertTextContext } from "../global-context/alert-text-context";
 
 // Shows alert message and disappears after a few  seconds
-const AlertText = ({ message, alertType }) => {
-  return <div></div>;
+const AlertText = () => {
+  const { alertText, setAlertText } = useAlertTextContext();
+  const { message, alertType } = alertText;
+
+  useEffect(
+    () =>
+      setTimeout(
+        () => setAlertText((prevObj) => ({ ...prevObj, isActive: false })),
+        5000
+      ),
+    []
+  );
+
+  return (
+    <small className={`alert alert-${alertType} fs-sml padding-sml`}>
+      <span>
+        <i
+          className={`fas ${
+            alertType === "success"
+              ? "fa-check-circle"
+              : "fa-exclamation-circle"
+          }`}
+        ></i>
+      </span>{" "}
+      {message}
+    </small>
+  );
 };
 
 export default AlertText;
